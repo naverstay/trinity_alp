22f9c9387624ac4a9c5ff987d51d73072b1e7351
nv <naverstay.me@gmail.com>
2020-11-25 12:52:07 +0200

    pages

--- pages.js
+++ pages.js
@@ -1,6 +1,17 @@
-const pages = [
-    'index',
-    'test'
-];
+//const pages = [
+//    'index',
+//    'test'
+//];
+
+let pages = [];
+const PATHS = require('./config/paths');
+const rootPagesFolderName = 'pug/pages';
+const fs = require('fs');
+
+const viewsFolder = PATHS.source + `/${rootPagesFolderName}/`;
+
+fs.readdirSync(viewsFolder).forEach(view => {
+    pages.push(view);
+});
 
 module.exports = pages;
--- source/pug/pages/index/index.js
+++ source/pug/pages/index/index.js
@@ -1,3 +1,4 @@
+import './index.sass';
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
