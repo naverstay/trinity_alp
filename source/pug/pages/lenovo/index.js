1220e66c23b4f1268b4c6aac263baff75bd0716b
nv <naverstay.me@gmail.com>
2020-11-26 14:16:44 +0200

    lenovo page 3

--- source/pug/pages/lenovo/index.js
+++ source/pug/pages/lenovo/index.js
@@ -1,9 +1,60 @@
 import './index.sass';
+import 'swiper/swiper.scss';
+
+import {
+    Swiper,
+    Navigation,
+    Pagination,
+    Scrollbar,
+    EffectCoverflow
+} from 'swiper/swiper.esm.js';
+
+console.log('Swiper', Swiper);
 
 document.addEventListener('DOMContentLoaded', (event) => {
+    let prodThumbSwiper;
+    let prodMainSwiper;
+    let skip = false;
+    let sliderSpeed = 500;
 
-    const text = 'Push me';
-    const satisfaction = `${text}, and then just touch me`;
+    prodThumbSwiper = new Swiper('.prodThumbSlider', {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        speed: sliderSpeed,
+        loop: true,
+        setWrapperSize: true,
+        watchSlidesProgress: true,
+        slideToClickedSlide: true,
+        on: {
+            slideChange: function (swp) {
+                console.log('prodThumbSwiper', swp);
 
-    (() => console.log(satisfaction))();
+                if (!skip && prodMainSwiper) {
+                    skip = true;
+                    prodMainSwiper.slideTo(swp.activeIndex);
+                }
+            },
+            slideChangeTransitionEnd: function () {
+                skip = false
+            }
+        }
+    });
+
+    prodMainSwiper = new Swiper('.prodMainSlider', {
+        loop: true,
+        setWrapperSize: true,
+        speed: sliderSpeed,
+        on: {
+            slideChange: function (swp) {
+                if (!skip && prodThumbSwiper) {
+                    skip = true;
+                    prodThumbSwiper.slideTo(swp.activeIndex);
+                }
+            },
+            slideChangeTransitionEnd: function () {
+                skip = false
+            }
+        }
+    });
+
 });
